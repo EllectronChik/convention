7af7f765b836aa87097ebd1d7931a17c4f0a11b0
EllectronChik <ellectronchik@proton.me>
2026-03-01 15:34:57 +0300

    refactor: remove duplicate versioning dependant ids

--- src/main/kotlin/convention/versioning/internal/constants/DependantIds.kt
+++
@@ -1,7 +0,0 @@
-package dev.ellectronchik.convention.versioning.internal.constants
-
-object DependantIds {
-    const val ANDROID_APP = "com.android.application"
-    const val ANDROID_LIBRARY = "com.android.library"
-    const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
-}
