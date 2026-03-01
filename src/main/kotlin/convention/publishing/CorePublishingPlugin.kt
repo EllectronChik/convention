a470765c8279fba6d79e12077572c5b4d4c4bf22
EllectronChik <ellectronchik@proton.me>
2026-03-01 16:17:25 +0300

    feat: enforce root-only core publishing plugin

--- src/main/kotlin/convention/publishing/CorePublishingPlugin.kt
+++ src/main/kotlin/convention/publishing/CorePublishingPlugin.kt
@@ -1,11 +1,16 @@
 package dev.ellectronchik.convention.publishing
 
 import dev.ellectronchik.convention.publishing.dsl.CorePublishingExtension
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class CorePublishingPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        if (target != target.rootProject) {
+            throw GradleException("CorePublishingPlugin must be applied only to the root project")
+        }
+
         target.project.extensions.create(
             "corePublishing",
             CorePublishingExtension::class.java,
