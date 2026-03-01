714b3bc0b00a6d3207102d68414607f8cc4dce8d
EllectronChik <ellectronchik@proton.me>
2026-03-01 15:34:40 +0300

    feat: add publishing plugin ids

---
+++ src/main/kotlin/convention/publishing/internal/constants/PluginProps.kt
@@ -0,0 +1,6 @@
+package dev.ellectronchik.convention.publishing.internal.constants
+
+object PluginProps {
+    const val CORE_PLUGIN_ID = "dev.ellectronchik.publishing.config"
+    const val MODULE_PLUGIN_ID = "dev.ellectronchik.publishing"
+}
