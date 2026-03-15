e3eab8e6ad50930e544dbd116854a85e2c4c2a1c
EllectronChik <ellectronchik@proton.me>
2026-03-15 13:29:38 +0300

    Extract extension names into publishing PluginProps constants

--- src/main/kotlin/convention/publishing/internal/constants/PluginProps.kt
+++ src/main/kotlin/convention/publishing/internal/constants/PluginProps.kt
@@ -1,6 +1,8 @@
 package dev.ellectronchik.convention.publishing.internal.constants
 
 internal object PluginProps {
+    const val CORE_EXTENSION_NAME = "corePublishing"
+    const val MODULE_EXTENSION_NAME = "modulePublishing"
+
     const val CORE_PLUGIN_ID = "dev.ellectronchik.publishing.config"
-    const val MODULE_PLUGIN_ID = "dev.ellectronchik.publishing"
 }
