7260cc560ff8a81c2ed54ae9362e1df463d7597c
EllectronChik <ellectronchik@proton.me>
2026-03-15 13:29:28 +0300

    Expand MissingPropertyException KDoc with @param documentation

--- src/main/kotlin/convention/versioning/exceptions/MissingPropertyException.kt
+++ src/main/kotlin/convention/versioning/exceptions/MissingPropertyException.kt
@@ -3,7 +3,12 @@ package dev.ellectronchik.convention.versioning.exceptions
 import dev.ellectronchik.convention.versioning.internal.constants.PluginProps
 import org.gradle.api.GradleException
 
-/** Thrown when a required `versioning { ... }` property is missing on the root project. */
+/**
+ * Thrown when a required `versioning { ... }` property is missing on the root project.
+ *
+ * @param propertyName the name of the missing property as it appears inside the `versioning` block.
+ * @param details optional extra context appended to the error message.
+ */
 class MissingPropertyException(
     propertyName: String,
     details: String = "",
