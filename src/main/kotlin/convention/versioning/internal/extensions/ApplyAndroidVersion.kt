28692a40af3444a12c3f096d1fd1bc318355fbb3
EllectronChik <ellectronchik@proton.me>
2026-04-23 12:40:09 +0300

    Replace `findByType` with `getByType` for `ApplicationAndroidComponentsExtension` in `applyAndroidVersion`

--- src/main/kotlin/convention/versioning/internal/extensions/ApplyAndroidVersion.kt
+++ src/main/kotlin/convention/versioning/internal/extensions/ApplyAndroidVersion.kt
@@ -1,17 +1,15 @@
 package dev.ellectronchik.convention.versioning.internal.extensions
 
-import com.android.build.api.dsl.ApplicationExtension
-import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import dev.ellectronchik.convention.versioning.dsl.VersioningExtension
 import dev.ellectronchik.convention.versioning.exceptions.MissingPropertyException
 import dev.ellectronchik.convention.versioning.internal.constants.PluginProps
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.findByType
 
 internal fun Project.applyAndroidVersion(extension: VersioningExtension) {
-    val androidComponents = this.extensions.findByType<AndroidComponentsExtension<*, *, *>>()
+    val androidComponents = this.extensions.getByType(ApplicationAndroidComponentsExtension::class.java)
 
-    androidComponents?.finalizeDsl { androidDsl ->
+    androidComponents.finalizeDsl { appExtension ->
         val overrideModuleVersion = extension.overrideModuleVersion.get()
         val currentVersionName =
             extension.currentVersionName.orNull
@@ -20,7 +18,7 @@ internal fun Project.applyAndroidVersion(extension: VersioningExtension) {
             extension.currentAndroidVersionCode.orNull
                 ?: throw MissingPropertyException(PluginProps.CURRENT_ANDROID_VERSION_PROPERTY)
 
-        (androidDsl as? ApplicationExtension)?.defaultConfig {
+        appExtension.defaultConfig {
             if (overrideModuleVersion) {
                 versionName = currentVersionName
                 versionCode = currentAndroidVersionCode
